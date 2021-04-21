7423d7bf5f6b2690d5b31ba88bd82ad4001dd5b2
fthx <23138504+fthx@users.noreply.github.com>
2021-04-21 22:07:33 +0200

    Refine the way to restore original hasOverview
    from @fmuellner

--- extension.js
+++ extension.js
@@ -1,55 +1,33 @@
 /*
   No overview at start-up
-  (c) fthx 2021
+  Contributors: @fthx, @fmuellner
   License: GPL v3
-  Legacy code is commented out for future possible flashback
 */
 
 
-const GLib = imports.gi.GLib;
 const Main = imports.ui.main;
-//const Overview = Main.overview;
 
 
 class Extension {
-	constructor() {
-		Main.sessionMode.hasOverview = false;
-		this._timeout = GLib.timeout_add_seconds(GLib.PRIORITY_DEFAULT, 2, () => {
-			Main.sessionMode.hasOverview = true;
-		});
-	}
+    constructor() {
+        this._realHasOverview = Main.sessionMode.hasOverview;
+    }
 
-	enable() {		
-	}
+    enable() {
+        if (!Main.layoutManager._startingUp) {
+            return;
+        }
 
-	disable() {
-		if (this._timeout) {
-			GLib.source_remove(this._timeout);
-		}
-	}
+        Main.sessionMode.hasOverview = false;
+        Main.layoutManager.connect('startup-complete', () => {
+            Main.sessionMode.hasOverview = this._realHasOverview
+        });
+    }
+
+    disable() {
+        Main.sessionMode.hasOverview = this._realHasOverview;
+    }
 }
-
-/*class Extension {
-	constructor() {
-		this.signal = Overview.connect('shown', this._hide.bind(this));
-	}
-	
-	_hide() {
-		Overview.hide();
-		if (this.signal) {
-				Overview.disconnect(this.signal);
-		}
-	}
-
-	enable() {
-	}
-
-	disable() {
-		if (this.signal && Overview.signalHandlerIsConnected(this.signal)) {
-			Overview.disconnect(this.signal);
-		}
-	}
-}*/
 
 function init() {
 	return new Extension();

8b50fc9949ec221f1fb95ab09aaed5ef1bb46d16
fthx <23138504+fthx@users.noreply.github.com>
2021-04-21 20:59:34 +0200

    New method for hiding
    ... that avoids the unwanted show/hide effect at start-up

--- extension.js
+++ extension.js
@@ -2,14 +2,34 @@
   No overview at start-up
   (c) fthx 2021
   License: GPL v3
+  Legacy code is commented out for future possible flashback
 */
 
 
+const GLib = imports.gi.GLib;
 const Main = imports.ui.main;
-const Overview = Main.overview;
+//const Overview = Main.overview;
 
 
 class Extension {
+	constructor() {
+		Main.sessionMode.hasOverview = false;
+		this._timeout = GLib.timeout_add_seconds(GLib.PRIORITY_DEFAULT, 2, () => {
+			Main.sessionMode.hasOverview = true;
+		});
+	}
+
+	enable() {		
+	}
+
+	disable() {
+		if (this._timeout) {
+			GLib.source_remove(this._timeout);
+		}
+	}
+}
+
+/*class Extension {
 	constructor() {
 		this.signal = Overview.connect('shown', this._hide.bind(this));
 	}
@@ -29,7 +49,7 @@ class Extension {
 			Overview.disconnect(this.signal);
 		}
 	}
-}
+}*/
 
 function init() {
 	return new Extension();

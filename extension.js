69d889c69b351245095f7693bb194f6cbf5806f6
icedman <m4rvin2005@gmail.com>
2021-05-11 15:23:47 +0800

    clean up enable/disable

--- extension.js
+++ extension.js
@@ -33,30 +33,35 @@ const setInterval = (func, delay, ...args) => {
 class Extension {
     constructor() {
         this._settings = ExtensionUtils.getSettings(schema_id);
-        this.vertical = false;
-        
         this.shrink = this._settings.get_boolean(SettingsKey.SHRINK_ICONS);
+        this.animateIcons = this._settings.get_boolean(SettingsKey.ANIMATE_ICONS);
+        this.bgDark = this._settings.get_boolean(SettingsKey.BG_DARK);
+        this.bgOpacity = this._settings.get_boolean(SettingsKey.BG_OPACITY);
         this.recycleOldDash = this._settings.get_boolean(SettingsKey.REUSE_DASH);
+        this.vertical = false;
 
         Main.overview.connect('showing', this._onOverviewShowing.bind(this));
         Main.overview.connect('hidden', this._onOverviewHidden.bind(this));
 
-        this._settings.connect(`changed::${SettingsKey.BG_OPACITY}`, () => {
-            this.dash.first_child.opacity = 255 * this._settings.get_double(SettingsKey.BG_OPACITY);
+        this._settings.connect(`changed::${SettingsKey.REUSE_DASH}`, () => {
+            this.recycleOldDash = this._settings.get_boolean(SettingsKey.REUSE_DASH);
+            this.disable();
+            this.enable();
         });
         this._settings.connect(`changed::${SettingsKey.BG_DARK}`, () => {
-            if (this._settings.get_boolean(SettingsKey.BG_DARK)) {
-                this.dashContainer.add_style_class_name('dark');
-            } else {
-                this.dashContainer.remove_style_class_name('dark');
-            }
+            this.bgDark = this._settings.get_boolean(SettingsKey.BG_DARK);
+            this._updateBgDark();
+        });
+        this._settings.connect(`changed::${SettingsKey.BG_OPACITY}`, () => {
+            this.bgOpacity = this._settings.get_boolean(SettingsKey.BG_OPACITY);
+            this._updateBgOpacity();
         });
         this._settings.connect(`changed::${SettingsKey.ANIMATE_ICONS}`, () => {
-            if (this._settings.get_boolean(SettingsKey.ANIMATE_ICONS)) {
-                this._enable_animation();
-            } else {
-                this._disable_animation();
-            }
+            this.animateIcons = this._settings.get_boolean(SettingsKey.ANIMATE_ICONS);
+        });
+        this._settings.connect(`changed::${SettingsKey.SHRINK_ICONS}`, () => {
+            this.shrink = this._settings.get_boolean(SettingsKey.SHRINK_ICONS);
+            this._updateShrink();
         });
     }
 
@@ -74,8 +79,7 @@ class Extension {
         }
 
         this.dash.showAppsButton.hide();
-        this.dashContainer = new St.BoxLayout({ name: 'dashContainer',
-                                           vertical: true });
+        this.dashContainer = new St.BoxLayout({ name: 'dashContainer', vertical: true });
 
         Main.layoutManager.addChrome(
             this.dashContainer,
@@ -83,24 +87,8 @@ class Extension {
                 trackFullscreen: true
             });
 
-        let [sw, sh] = global.display.get_size();
-
-        // this.dockWidth = 80;
-        this.dockHeight = this.shrink ? 80 : 110;
-
-        this.animationContainer = new St.Widget();
-
-        // layout
-        if (this.vertical) {
-            this.dashContainer.set_position(0, 0);
-            this.dash.last_child.vertical = true;
-            this.dash.last_child.first_child.layout_manager.orientation = 1;
-            this.dashContainer.set_height(sh);
-        } else {
-            this.dashContainer.set_position(0, sh - this.dockHeight);
-            this.dashContainer.set_width(sw);
-            this.dashContainer.set_height(this.dockHeight);
-        }
+        this.animationContainer = new St.Widget({ name: 'animationContainer' });
+        Main.uiGroup.add_child(this.animationContainer);
 
         if (this.recycleOldDash) {         
             Main.uiGroup.find_child_by_name('overview').first_child.remove_child(this.dash);
@@ -112,29 +100,22 @@ class Extension {
             Main.overview.dash.height = 0;
             Main.overview.dash.hide();
         }
-
-        if (this.shrink) {
-            this.dashContainer.add_style_class_name('shrink');
-        } else {
-            this.dashContainer.remove_style_class_name('shrink');
-        }
-        if (this._settings.get_boolean(SettingsKey.BG_DARK)) {
-            this.dashContainer.add_style_class_name('dark');
-        } else {
-            this.dashContainer.remove_style_class_name('dark');
-        }        
-
-        // theme        
-        this.dash.first_child.opacity = 255 * this._settings.get_double(SettingsKey.BG_OPACITY);
-
         this.dashContainer.add_child(this.dash);
-        Main.uiGroup.add_child(this.animationContainer);
 
-        this.animationContainer.hide();
-        this._enable_animation();
+        this._updateShrink();
+        this._updateBgDark();
+        this._updateBgOpacity();
+        this._updateLayout();
+        this._updateAnimation();
     }
 
     disable() {
+        this._updateShrink(true);
+        this._updateBgDark(true);
+        this._updateBgOpacity(true);
+        this._updateLayout(true);
+        this._updateAnimation(true);
+
         this.dash.showAppsButton.hide();
         if (this.recycleOldDash) {
             this.dashContainer.remove_child(this.dash);
@@ -148,23 +129,54 @@ class Extension {
         delete this.dashContainer;
     }
 
-    _onOverviewShowing() {
-        this._inOverview = true;
-        this.dashContainer.height = 0;
-        this.animationContainer.hide();
-        this.dash.hide();
+    _updateShrink(disable) {
+        if (this.shrink && !disable) {
+            this.dashContainer.add_style_class_name('shrink');
+        } else {
+            this.dashContainer.remove_style_class_name('shrink');
+        }
+        this._updateLayout(disable);
     }
 
-    _onOverviewHidden() {
-        this._inOverview = false;
-        this.dashContainer.height = this.dockHeight;
-        this.animationContainer.show();
-        this.dash.show();
+    _updateBgDark(disable) {
+        if (this.bgDark && !disable) {
+            this.dashContainer.add_style_class_name('dark');
+        } else {
+            this.dashContainer.remove_style_class_name('dark');
+        }        
+    }
+
+    _updateBgOpacity(disable) {
+        if (disable) {
+            this.dash.first_child.opacity = 255 * this._bgOpacity;
+        } else {
+            this.dash.first_child.opacity = 255;
+        }
+    }
+
+    _updateLayout(disable) {
+        let [sw, sh] = global.display.get_size();
+        this.dockHeight = (this.shrink && !disable) ? 80 : 110;
+        if (this.vertical) {
+            this.dashContainer.set_position(0, 0);
+            this.dash.last_child.vertical = true;
+            this.dash.last_child.first_child.layout_manager.orientation = 1;
+            this.dashContainer.set_height(sh);
+        } else {
+            this.dashContainer.set_position(0, sh - this.dockHeight);
+            this.dashContainer.set_width(sw);
+            this.dashContainer.set_height(this.dockHeight);
+        }        
+    }
+
+    _updateAnimation() {
+
     }
 
     _enable_animation() {
         this._animationOn = true;
-        if (!this._settings.get_boolean(SettingsKey.ANIMATE_ICONS) || this._animationControllerSet) {
+
+        if (!this._settings.get_boolean(SettingsKey.ANIMATE_ICONS)) {
             return;
         }
 
@@ -173,31 +185,42 @@ class Extension {
         this._animateOut = 0;
         this._inDash = false;
 
-        setInterval(() => {
-            this._animate();
-        }, 15);
+        // setInterval(() => {
+        //     if (!this._inOverview) {
+        //         this.animationContainer.show();
+        //     } else {
+        //         this.animationContainer.hide();
+        //     }           
+        // }, 15);
 
         this.dashContainer.connect('motion-event', (actor, event, motion) => {
             this._animateOut = 0;
+            this._animate();
         });
 
         this.dashContainer.connect('enter-event', () => {
             this._animateOut = 0;
             this._inDash = true;
+            // this.dashContainer.add_style_class_name('hi');
         });
 
         this.dashContainer.connect('leave-event', () => {
             this._animateOut = 8;
             this._inDash = false;
+            for(let i=15; i<200; i+=15) {
+                setTimeout(() => {
+                    this._animate()
+                }, i);
+            }
+            // this.dashContainer.remove_style_class_name('hi');
         });
 
         this._animate(true);
-
-        this._animationControllerSet = true;
     }
 
     _disable_animation() {
         this._animationOn = false;
+        this.animationContainer.hide();
     }
 
     _animate(animateOut) {
@@ -212,8 +235,7 @@ class Extension {
         this.animationContainer.size = this.dashContainer.size;
 
         let X = Dash.last_child.x;
-        this.Y = this.Y || Dash.last_child.y;
-        let Y = this.Y;
+        let Y = 0;
 
         if (X == 0) return;
 
@@ -280,18 +302,26 @@ class Extension {
 
         });
 
-        if (!this._inOverview) {
-            this.animationContainer.show();
-        } else {
-            this.animationContainer.hide();
-        }
-
         this.animationContainer.get_children().forEach(c => {
             if (c._orphan) {
                 this.animationContainer.remove_child(c);
             }
         })
     }
+
+    _onOverviewShowing() {
+        this._inOverview = true;
+        this.dashContainer.height = 0;
+        this.animationContainer.hide();
+        this.dash.hide();
+    }
+
+    _onOverviewHidden() {
+        this._inOverview = false;
+        this.dashContainer.height = this.dockHeight;
+        this.animationContainer.show();
+        this.dash.show();
+    }    
 }
 
 function init() {

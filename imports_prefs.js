d0f37d70d79a99995e0e5eaba90d0f9bc40c9946
icedman <m4rvin2005@gmail.com>
2024-03-30 23:27:04 +0800

    g44 - move icon search path to fillPreferencesWindow

--- imports_prefs.js
+++ imports_prefs.js
@@ -1,10 +1,10 @@
 function init() {
   ExtensionUtils.initTranslations();
-  let iconTheme = Gtk.IconTheme.get_for_display(Gdk.Display.get_default());
-  iconTheme.add_search_path(`${Me.dir.get_path()}/ui/icons`);
 }
 
 function fillPreferencesWindow(window) {
+  let iconTheme = Gtk.IconTheme.get_for_display(Gdk.Display.get_default());
+  iconTheme.add_search_path(`${Me.dir.get_path()}/ui/icons`);
   let p = new Preferences();
   p.path = Me.dir.get_path();
   p.fillPreferencesWindow(window);
